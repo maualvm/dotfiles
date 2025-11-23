4120c91c2903c95758793c2e773f51503fb39d9a
Mauricio Alvarez <maualvm@gmail.com>
2025-11-23 12:32:30 -0600

    Add fzf-lua keymaps

--- .config/nvim/lua/plugins/fzf-lua.lua
+++ .config/nvim/lua/plugins/fzf-lua.lua
@@ -3,4 +3,9 @@ return {
   dependencies = { "nvim-tree/nvim-web-devicons" },
   opts = {},
   lazy = false,
+  keys = {
+    { "<leader>fz", ":FzfLua<CR>",              silent = true, desc = "Open FzfLua" },
+    { "<leader>ff", ":FzfLua files<CR>",        silent = true, desc = "Open file picker" },
+    { "<leader>fg", ":FzfLua grep_visual<CR>",  silent = true, desc = "Grep for text" },
+  },
 }
