138146397937911414ad95f56c130172d73c81e2
Mauricio Alvarez <maualvm@gmail.com>
2025-11-23 12:32:15 -0600

    Add oil keymaps

--- .config/nvim/lua/plugins/oil.lua
+++ .config/nvim/lua/plugins/oil.lua
@@ -8,7 +8,7 @@ return {
   dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
   lazy = false,
   keys = {
-    { "<leader>o", "<cmd>lua require('oil').open()<CR>" },
-    { "<leader>O", "<cmd>lua require('oil').open_float()<CR>" }
+    { "<leader>o", "<cmd>lua require('oil').open()<CR>",        silent = true, desc = "Open file browser" },
+    { "<leader>O", "<cmd>lua require('oil').open_float()<CR>",  silent = true, desc = "Open file browser in a floating window" }
   }
 }
