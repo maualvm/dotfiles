6f587f88ef043183520ec178482dca5ac4a8d0ae
Mauricio Alvarez <maualvm@gmail.com>
2025-11-23 14:22:03 -0600

    Add oldfiles keymap

--- .config/nvim/lua/plugins/fzf-lua.lua
+++ .config/nvim/lua/plugins/fzf-lua.lua
@@ -3,8 +3,9 @@ return {
   dependencies = { "nvim-tree/nvim-web-devicons" },
   opts = {},
   keys = {
-    { "<leader>fz", ":FzfLua<CR>",              silent = true, desc = "Open FzfLua" },
-    { "<leader>ff", ":FzfLua files<CR>",        silent = true, desc = "Open file picker" },
-    { "<leader>fg", ":FzfLua grep_visual<CR>",  silent = true, desc = "Grep for text" },
+    { "<leader>fz", ":FzfLua<CR>",            silent = true, desc = "Open FzfLua" },
+    { "<leader>ff", ":FzfLua files<CR>",      silent = true, desc = "Open file picker" },
+    { "<leader>fg", ":FzfLua live_grep<CR>",  silent = true, desc = "Fuzzy/Regex search" },
+    { "<leader>fr", ":FzfLua oldfiles<CR>",   silent = true, desc = "Open recent files" },
   },
 }
