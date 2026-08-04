d2f9767a5f9153329a8e81fa3ebbf9c0a1d5e559
Mauricio Alvarez <maualvm@gmail.com>
2026-08-04 17:37:38 -0600

    Add hunk preview keymaps for gitsigns

--- .config/nvim/lua/plugins/gitsigns.lua
+++ .config/nvim/lua/plugins/gitsigns.lua
@@ -6,7 +6,9 @@ return {
     current_line_blame = true
   },
   keys = {
-    { "[g", "<cmd>lua require('gitsigns').prev_hunk()<CR>", silent = true, desc = "Go to previous hunk" },
-    { "]g", "<cmd>lua require('gitsigns').next_hunk()<CR>", silent = true, desc = "Go to next hunk" },
+    { "[g",         "<cmd>lua require('gitsigns').prev_hunk()<CR>",            silent = true, desc = "Go to previous hunk" },
+    { "]g",         "<cmd>lua require('gitsigns').next_hunk()<CR>",            silent = true, desc = "Go to next hunk" },
+    { "<leader>hp", "<cmd> lua require('gitsigns').preview_hunk()<CR>",        silent = true, desc = "Preview hunk" },
+    { "<leader>hi", "<cmd> lua require('gitsigns').preview_hunk_inline()<CR>", silent = true, desc = "Preview hunk inline" },
   }
 }
